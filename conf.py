b245c5bc593ddd5bb5bd8dc5a1dcc04365cab374
Zalán Meggyesi <zmeggyesi@skawa.hu>
2016-05-04 08:41:07 +0200

    Everything deleted but the global

--- conf.py
+++ conf.py
@@ -1,10 +1,5 @@
 #-*- coding: utf-8 -*-
 
-import sys
-import os.path
-from six import string_types
-from sphinx import version_info
-
 html_context = {
-    "display_github": False
+    "display_github": False,
 }
