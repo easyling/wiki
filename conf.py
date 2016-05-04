40e32e55bdb8b265a5daab345365987e4d40ce09
Zalán Meggyesi <zmeggyesi@skawa.hu>
2016-05-04 10:25:48 +0200

    New attempt

--- conf.py
+++ conf.py
@@ -1,3 +1,8 @@
 html_context = {
+    'github_version': 'breadcrumbs',
     'display_github': False,
+    'bitbucket_user': 'None',
+    'bitbucket_repo': 'None',
+    'bitbucket_version': 'breadcrumbs',
+    'display_bitbucket': False,
 }
