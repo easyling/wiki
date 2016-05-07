5bb6db0db99a29042980dce7d5450a37f491f45f
Zalan Meggyesi <zmeggyesi@skawa.hu>
2016-05-07 16:45:03 +0200

    Override theme path directly

--- conf.py
+++ conf.py
@@ -1,8 +1,1 @@
-html_context = {
-    'github_version': 'breadcrumbs',
-    'display_github': False,
-    'bitbucket_user': 'None',
-    'bitbucket_repo': 'None',
-    'bitbucket_version': 'breadcrumbs',
-    'display_bitbucket': False,
-}
+html_theme_path = './templates'
